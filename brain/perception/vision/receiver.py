8cca0758a0dde73615b76d2be38c0f60c7b89f7b
Kevin Chavez <kevin.j.chavez@gmail.com>
2016-10-17 21:57:38 -0700

    remove dead code

--- brain/perception/vision/receiver.py
+++ brain/perception/vision/receiver.py
@@ -16,35 +16,3 @@ class ImageMemory(ThoughtProcess):
         print("Perceived an image with mean %f" %
               (np.mean(perceived_image.image),))
 
-
-
-"""
-def create_bound_subcriber(address):
-    context = zmq.Context()
-    sock = context.socket(zmq.SUB)
-    sock.setsockopt(zmq.SUBSCRIBE, '')
-    sock.bind(address)
-    return sock
-
-class VisionHandler(object):
-    def __init__(self, address):
-        self.address = address
-        self.emitter = None
-
-    def handle(self, data):
-        perceived_image = PerceivedImage(data)
-        print "Received an image:"
-        print perceived_image.image
-        if self.emitter is not None:
-            action = Action()
-            action.display_text.text = "Received an image."
-            self.emitter.send(action.SerializeToString())
-
-    def start(self):
-        self.sock = create_bound_subcriber(self.address)
-        self.emitter = ActionEmitter.new_publisher()
-        print_header("Running Vision Handler")
-        while True:
-            data = self.sock.recv()
-            self.handle(data)
-"""
